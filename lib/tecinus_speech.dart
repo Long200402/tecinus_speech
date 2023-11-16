eed69bf455dd05e5f2317696cd47daea6fbf64df
Long200402 <Long200402@gmail.com>
2023-11-16 14:07:39 +0700

    fix lb

--- lib/tecinus_speech.dart
+++ lib/tecinus_speech.dart
@@ -2,3 +2,13 @@
 library tecinus_speech;
 
 export 'package:tecinus_speech/tecinus_speech_widget.dart';
+export 'package:record/record.dart';
+export 'package:flutter/foundation.dart';
+export 'package:flutter/material.dart';
+export 'package:flutter_bloc/flutter_bloc.dart';
+export 'package:flutter_screenutil/flutter_screenutil.dart';
+export 'package:tecinus_speech/bloc/speech_bloc.dart';
+export 'package:tecinus_speech/widget/dialog_service.dart';
+export 'package:tecinus_speech/widget/item_list_speak.dart';
+export 'package:tecinus_speech/widget/progress_answer_widget.dart';
+export 'package:tecinus_speech/widget/recoder.dart';
--- lib/tecinus_speech_widget.dart
+++ lib/tecinus_speech_widget.dart
@@ -1,6 +1,3 @@
-
-library tecinus_speech;
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
